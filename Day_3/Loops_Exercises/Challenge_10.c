0fe2581d5d815f46a6aa471464146fb5ef965cff
dounia <asfordounia@gmail.com>
2024-09-13 10:11:50 +0100

    add Challenge_10

---
+++ Day_3/Loops_Exercises/Challenge_10.c
@@ -0,0 +1,37 @@
+// C program for the above approach 
+#include <math.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+
+
+
+
+// Driver Code 
+int main() 
+{ 
+
+	int N ; 
+	int i = 0; 
+
+
+	srand((unsigned int)(time(NULL))); 
+
+	char generateur[62] = "0123456789abcdefghijklmnoqprstuvwyzxABCDEFGHIJKLMNOQPRSTUYWVZX";
+
+    printf("Entrez le size de votre mot de passe : ");
+    scanf("%d",&N);
+	char password[N]; 
+
+
+	for (i = 0; i < N; i++) { 
+
+        password[i] = generateur[rand() % (sizeof(generateur) - 1)]; 
+
+	} 
+
+    password[N] = '\0';
+    printf("%s", password); 
+
+	return 0; 
+}
